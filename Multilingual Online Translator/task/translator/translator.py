d3187b9280bd2eff42a661041bab94f603ea5c01
denys <denamyte@gmail.com>
2023-02-01 16:17:53 +0200

    2023-02-01: Stage 1/7: finished

--- Multilingual Online Translator/task/translator/translator.py
+++ Multilingual Online Translator/task/translator/translator.py
@@ -1,1 +1,9 @@
-print('Hello, World!')
+def main():
+    lang = input('Type "en" if you want to translate from French into English,'
+                 ' or "fr" if you want to translate from English into French:\n')
+    word = input('Type the word you want to translate:\n')
+    print(f'You chose "{lang}" as the language to translate "{word}" to.')
+
+
+if __name__ == '__main__':
+    main()
